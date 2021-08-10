e7c0629ac836d9861e1db33fe59755a13d12ba15
gbvanrenswoude <gbvanrenswoude@gmail.com>
2021-08-10 14:36:13 +0200

    refactor: 💡 move to l3 construct not using aspects but cr

--- src/integ.default.ts
+++ src/integ.default.ts
@@ -1,8 +1,8 @@
-import { EcrAssetScanner } from "./index";
-// import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
+import { ScannedDockerImageAsset } from "./index";
+import { DockerImageAsset } from "@aws-cdk/aws-ecr-assets";
 import * as cdk from "@aws-cdk/core";
 import * as ecs from "@aws-cdk/aws-ecs";
-// import * as path from "path";
+import * as path from "path";
 
 const env = {
   region: process.env.CDK_DEFAULT_REGION,
@@ -16,14 +16,25 @@ export class TestStack extends cdk.Stack {
     super(scope, id, props);
 
     // Drop in some Docker Image Asset
-    // const asset = new DockerImageAsset(this, "whoo", {
-    //   directory: path.join(__dirname, "Dockerfile"),
-    // });
+    // FIXME type warnings in .fromDockerImageAsset()
+    const image = new DockerImageAsset(this, "whoo", {
+      directory: path.join(__dirname, "../src/"),
+    });
+    const image1 = ecs.ContainerImage.fromDockerImageAsset(image);
 
     // Container Image Asset
-    const image = ecs.ContainerImage.fromAsset("./src/", {
-      buildArgs: { TEST_VAR: "BAR" },
+    const image2 = ecs.ContainerImage.fromAsset("./src/");
+
+    // Image 3 with scan
+    const image3 = new ScannedDockerImageAsset(this, "whaa", {
+      directory: path.join(__dirname, "../src/"),
     });
+    const image31 = ecs.ContainerImage.fromDockerImageAsset(image3);
+    // Image 4 with scan
+    const image4 = new ScannedDockerImageAsset(this, "whaas", {
+      directory: path.join(__dirname, "../src/"),
+    });
+    const image41 = ecs.ContainerImage.fromDockerImageAsset(image4);
 
     const taskDefinition = new ecs.FargateTaskDefinition(
       this,
@@ -34,28 +45,41 @@ export class TestStack extends cdk.Stack {
       }
     );
     taskDefinition.addContainer("container", {
-      image,
+      image: image1,
       environment: {
-        TEST_VAR: "BAR",
+        TEST_VAR: "ONE",
       },
       logging: new ecs.AwsLogDriver({
-        streamPrefix: "testing",
+        streamPrefix: "testingone",
       }),
     });
-    // taskDefinition.addContainer("container_two", {
-    //   asset,
-    //   environment: {
-    //     TEST_VAR: "BAR",
-    //   },
-    //   logging: new ecs.AwsLogDriver({
-    //     streamPrefix: "testing",
-    //   }),
-    // });
-
-    new EcrAssetScanner(this, "test1", {
-      assetParameter: "huu",
-      assetName: "u",
-      severity: "NEVER",
+    taskDefinition.addContainer("container_two", {
+      // image: ecs.ContainerImage.fromEcrRepository(image1.repository),
+      image: image2,
+      environment: {
+        TEST_VAR: "TWO",
+      },
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: "testingtwo",
+      }),
+    });
+    taskDefinition.addContainer("container_three", {
+      image: image31,
+      environment: {
+        TEST_VAR: "THREE",
+      },
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: "testingthree",
+      }),
+    });
+    taskDefinition.addContainer("container_four", {
+      image: image41,
+      environment: {
+        TEST_VAR: "FOUR",
+      },
+      logging: new ecs.AwsLogDriver({
+        streamPrefix: "testingfour",
+      }),
     });
   }
 }
